b4d9f807414b092336e3353eada9bb17f874b130
luan-silva03 <luandersonfabricio03@gmail.com>
2023-03-17 13:19:42 -0300

    jsquaseterminado

---
+++ exercicios/aula12/ex012.js
@@ -0,0 +1,9 @@
+var hora = 18
+console.log(`São exatamento ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bom dia') 
+} else if (hora >= 12) {
+    console.log('Boa tarde')
+} else (hora > 18) {
+    console.log('Boa noite')
+}
