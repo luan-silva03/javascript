26eacafd4c7a53fe7daba99bf25633474c3a94c7
luan-silva03 <luandersonfabricio03@gmail.com>
2023-03-23 17:18:23 -0300

    ambijs

---
+++ Aulas/aula013/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 9) 
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
