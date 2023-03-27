664587b794b11df84d26d34a729770307f5af1ff
luan-silva03 <luandersonfabricio03@gmail.com>
2023-03-27 10:54:20 -0300

    jsfor

--- Aulas/aula013/ambiente.js
+++ Aulas/aula013/ambiente.js
@@ -1,13 +1,28 @@
-var c = 1
-do {
-    console.log(`passo ${c}`)
-    c++
-} while (c <= 9) 
-
-
 /*
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 */
+
+/*
+var c = 1
+    while (c <= 9) {
+    console.log(`passo ${c}`)
+    c++
+}  
+*/
+
+/*
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 9) 
+*/
+
+
+
+for (var c = 1;c <= 7;c++) {
+    console.log(c)
+}
